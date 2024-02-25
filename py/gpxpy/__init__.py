a7c26757ba18bbc646c05004d945240be0f7f6da
Danfro <one@frostinfo.de>
2024-02-25 16:51:29 +0100

    add link to source repository

--- py/gpxpy/__init__.py
+++ py/gpxpy/__init__.py
@@ -13,6 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# source: https://github.com/tkrajina/gpxpy
 
 
 def parse(xml_or_file, parser=None):
