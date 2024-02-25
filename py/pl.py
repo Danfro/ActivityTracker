a62a4d3a277cf641f8dd89f78ec50a446aa3bbdd
Danfro <one@frostinfo.de>
2024-02-25 14:05:20 +0100

    add link to source repository

--- py/pl.py
+++ py/pl.py
@@ -16,6 +16,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+# source: https://github.com/astanin/gpxplot
 
 """usage: gpxplot.py [action] [options] track.gpx
 
